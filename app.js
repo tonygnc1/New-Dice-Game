75e6c33a0f577fc057e4297305f3b907093c1910
Tony <bootcamp1955@gmail.com>
2019-11-25 14:10:21 -0500

    style.css

---
+++ app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+button.addEventListener('click', function() {
+    let rollDice = roll(6);
+    console.log(rollDice);
+    let holder = builder(rollDice);
+    output.appendChild(holder);
+})
+
+function builder(num) {
+    let div = document.createElement('div');
+    div.setAttribute('class', 'dicer');
+    return div;
+}
+
+function roll(num) {
+    let rNumber = Math.floor(Math.random() * num) +1;
+    return rNumber;
+}
