e085f9364c8cf1122530abb64fede55906d15245
Tony <bootcamp1955@gmail.com>
2019-11-26 12:04:35 -0500

    App.js

--- app.js
+++ app.js
@@ -1,30 +1,51 @@
 const button = document.querySelector('button');
 const output = document.querySelector('.output');
+const player1 = document.querySelector('#player1');
+const player2 = document.querySelector('#player2');
 const dice = [[5], [1, 9], [1, 5, 9], [1, 3, 7, 9], [1, 3, 5, 7, 9], [1, 3, 4, 6, 7, 9]];
 button.addEventListener('click', function () {
-    let rollDice = roll(6);
+    let rolls = [roll(6), roll(6)];
+    let temp;
+if(rolls[0] == rolls[1]) { temp = 'Draw';}
+else if(rolls[0] > rolls[1]) { temp = 'Player 1 Wins';}
+else { temp = 'Player 2 Wins';}
+updateOutput(player1, rolls[0]);
+updateOutput(player2, rolls[1]);
+output.innerHTML = temp;
+
     // console.log(rollDice);
-    let holder = builder(rollDice);
-    console.log(output.children[0]);
-    if (output.children[0]) { output.children[0].remove(); }
-    output.appendChild(holder);
+   
 })
+
+function updateOutput(el, num) {
+    let holder = builder(num);
+    if(el.children[0]) {el.children[0].remove()}
+    el.appendChild(holder);
+}
 
 function builder(num) {
     let div = document.createElement('div');
     let dieArray = dice[num - 1];
+
+
     console.log(dieArray);
     for (let x = 1; x < 10; x++) {
         let span = document.createElement('div');
         span.setAttribute('class', 'dot');
+
         if (dieArray.includes(x)) {
             span.classList.add('black');
+
         }
+
         div.appendChild(span);
     }
     div.setAttribute('class', 'dicer');
-    return div;
+return div;
+
 }
+
+
 
 function roll(num) {
     let rNumber = Math.floor(Math.random() * num) + 1;
